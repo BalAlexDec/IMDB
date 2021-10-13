e12a60e3fb3140e2554a7759bc22b7f7ae8e49e8
Roherky <rodrigo31996@gmail.com>
2021-10-13 17:56:49 +0200

    Subida final archivo Main

--- Main.js
+++ Main.js
@@ -1,9 +1,17 @@
 "use strict";
 exports.__esModule = true;
 var Professional_1 = require("./Professional");
+var Movie_1 = require("./Movie");
+// Main
 var primerActor = new Professional_1.Professional("Antonio Banderas", 54, "hombre", 74, 1.74, "negro", "marrones", "blanca", false, "española", 2, "actor");
 var segundoActor = new Professional_1.Professional("Scarlett Johansson", 41, "mujer", 60, 1.64, "rubio", "azul", "blanca", false, "estadounidense", 2, "actriz");
 var tercerActor = new Professional_1.Professional("Manolo Lama", 57, "hombre", 75, 1.77, "blanco", "marrones", "blanca", true, "española", 5, "comentarista");
 primerActor.printAll();
 segundoActor.printAll();
 tercerActor.printAll();
+var primeraPelicula = new Movie_1.Movie("V de Vendetta", 2005, "estadounidense");
+var segundaPelicula = new Movie_1.Movie("A Todo de Gas", 1997, "estadounidense");
+var tercerPelicula = new Movie_1.Movie("Harry Potter y la Piedra Filosofal", 1996, "estadounidense");
+primeraPelicula.printallMovie();
+segundaPelicula.printallMovie();
+tercerPelicula.printallMovie();
--- Main.ts
+++ Main.ts
@@ -1,4 +1,9 @@
 import { Professional } from "./Professional";
+import { Movie } from "./Movie";
+
+
+
+// Main
 
 let primerActor = new Professional("Antonio Banderas", 54, "hombre", 74, 1.74, "negro", "marrones", "blanca", false, "española", 2, "actor");
 let segundoActor = new Professional("Scarlett Johansson", 41, "mujer", 60, 1.64, "rubio", "azul", "blanca", false, "estadounidense", 2, "actriz");
@@ -7,3 +12,11 @@ let tercerActor = new Professional("Manolo Lama", 57, "hombre", 75, 1.77, "blanc
 primerActor.printAll();
 segundoActor.printAll();
 tercerActor.printAll();
+
+let primeraPelicula = new Movie("V de Vendetta", 2005, "estadounidense");
+let segundaPelicula = new Movie("A Todo de Gas", 1997, "estadounidense");
+let tercerPelicula = new Movie("Harry Potter y la Piedra Filosofal", 1996, "estadounidense");
+
+primeraPelicula.printallMovie();
+segundaPelicula.printallMovie();
+tercerPelicula.printallMovie();
