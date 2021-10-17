7e312f576551642e611524154cf22fe86e1d153b
Roherky <rodrigo31996@gmail.com>
2021-10-17 23:48:14 +0200

    Final commit

--- Movie.ts
+++ Movie.ts
@@ -7,22 +7,25 @@ class Movie {
     director:string;
     writert:string;
     lenguage:string;
+    platform:string;
+    isMCU:boolean;
     mainCharacterName:string;
     produced:string;
     disttributor:string;
     genre:string;
 
-    constructor(title:string,releseYear:number,nacionality:string){
+    constructor(title:string,releseYear:number,nacionality:string,genre:string){
         this.title=title;
         this.releseYear=releseYear;
         this.nacionality=nacionality;
+        this.genre=genre;
     }
 
     printallMovie(){
         console.log("Nombre  "+this.title+" "+"releseYear  "+this.releseYear+
         "actors  "+this.actors+" "+"nacionality  "+this.nacionality+" "+"director  "+this.director+" "+"writert  "+this.writert+" "+
         "lenguage  "+this.lenguage+" "+"mainCharacterName  "+this.mainCharacterName+" "+"produced  "+this.produced+" "+"disttributor  "+this.disttributor+" "
-        +"genre  "+this.genre+" ");
+        +"genre  "+this.genre+" " +"platform  "+this.platform+" "+"isMCU  "+this.isMCU+" ");
     }
 }
 export{Movie};
